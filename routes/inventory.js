91fc129b9fbee032c06dc942950f76d43611cc00
Be-bo <robfiker@gmail.com>
2023-11-08 08:20:22 -0800

    Get Inventories API request finished

--- routes/inventory.js
+++ routes/inventory.js
@@ -4,10 +4,12 @@ const knex = require('knex')(require('../knexfile.js'));
 
 router.get('/inventories', async(req, res) => {
     try{
-        const data = await knex.select('*').from('inventories');
-        console.log(data);
+        const allInventoriesData = await knex.select('*').from('inventories');
+        console.log('Responded with a list of all inventories successfully.');
+        res.json(allInventoriesData);
     }catch(error){
-        console.log('Bruh, en error: ', error);
+        console.log('Cannot return a list of all inventories: ', error);
+        res.status(500).json({ error });
     }
 });
 
