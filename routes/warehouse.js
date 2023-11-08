ba3f5aca6fd60e53471dcdff189773659187382a
daaniyaal <daaniyaalmirza@gmail.com>
2023-11-08 11:31:20 -0800

    added put and delete

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -26,4 +26,43 @@ router.post("/warehouse", async (req, res) => {
   }
 });
 
+router.get("/warehouse/:id", async (req, res) => {
+  try {
+    const id = req.params.id; // Get the ID from the URL parameter
+    const oneWarehouseData = await knex
+      .select("*")
+      .from("warehouses")
+      .where({ id });
+
+    if (oneWarehouseData.length === 0) {
+      console.log("Warehouse not found");
+      res.status(404).json({ error: "Warehouse not found" });
+    } else {
+      console.log("Responded with selected warehouse");
+      res.json(oneWarehouseData[0]); // Assuming there's only one matching warehouse
+    }
+  } catch (error) {
+    console.log("Cannot return a warehouse: ", error);
+    res.status(500).json({ error });
+  }
+});
+router.delete("/warehouse/:id", async (req, res) => {
+  try {
+    await knex("warehouses").where({ id: req.params.id }).del();
+
+    res.sendStatus(204);
+  } catch (error) {
+    console.log("Cannot return a warehouse: ", error);
+    res.status(500).json({ error });
+  }
+});
+
+router.put("/warehouse/:id", async (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  const data = await knex("warehouses")
+    .where({ id: req.params.id })
+    .update(req.body);
+  res.json(data);
+});
 module.exports = router;
