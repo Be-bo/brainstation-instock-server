6739fdf838ee44d05fc6fa835c614e2fec718d9e
Be-bo <robfiker@gmail.com>
2023-11-08 08:09:58 -0800

    Initial inventories request working

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('knex')(require('../knexfile.js'));
 
-router.get('/warehouses', async(req, res) => {
-    // TODO: Robert
+router.get('/inventories', async(req, res) => {
+    try{
+        const data = await knex.select('*').from('inventories');
+        console.log(data);
+    }catch(error){
+        console.log('Bruh, en error: ', error);
+    }
 });
 
 module.exports = router;
